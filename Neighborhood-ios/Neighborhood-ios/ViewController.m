fa347b8213deff4b17ef160b6cf9b98de0ff00ec
tulindanil <tulindanil@gmail.com>
2015-08-25 00:07:18 +0800

    Added logic + simple loop

--- Neighborhood-ios/Neighborhood-ios/ViewController.m
+++ Neighborhood-ios/Neighborhood-ios/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "UITemperatureView.h"
+
+#import <Parse/Parse.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UITemperatureView *temperatureView;
 
 @end
 
@@ -18,12 +23,57 @@
 {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.temperatureView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.temperatureView sizeToFit];
+    self.temperatureView.center = CGPointMake(CGRectGetMidX(self.view.frame), CGRectGetMidY(self.view.frame));
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), ^{
+        
+        [Parse setApplicationId:@"VOB4wXj2mGOjJaqzdhkM701n2ahTSRMqZW6QQ8XU" clientKey:@"XCPG2OTVrapoymNGS5XGQIhsRM3F2tnVUgaceyec"];
+        
+        while (1)
+        {
+            sleep(1);
+            PFQuery *query = [PFQuery queryWithClassName:@"Temperature"];
+            [query orderByDescending:@"createdAt"];
+            [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error){
+                [UIView animateWithDuration:.3f animations:^{
+                    self.temperatureView.value = [object[@"value"] floatValue];
+                }];
+            }];
+        }
+    });
+}
+
+#pragma mark - TemperatureView
+
+- (UITemperatureView *)temperatureView
+{
+    if (!_temperatureView)
+    {
+        _temperatureView = [[UITemperatureView alloc] init];
+    }
+    return _temperatureView;
+}
+
+#pragma mark - Status Bar
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleDefault;
 }
 
 @end
