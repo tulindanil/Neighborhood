8dd95f98f78df1a63225710ee17a2128d44e2400
tulindanil <tulindanil@gmail.com>
2015-08-24 22:36:29 +0800

    column name fix

--- Neighborhood-raspberry/main.c
+++ Neighborhood-raspberry/main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
         {
             float currentTemp = getTemperature();
             char data[256];
-            sprintf(data, "{ \"temperature\": %f }", currentTemp);
+            sprintf(data, "{ \"value\": %f }", currentTemp);
             parseSendRequest(client, "POST", "/1/classes/Temperature", data, NULL);
         }
     }
