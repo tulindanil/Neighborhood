4d7df9e367365226f36d87c1189847b286859275
tulindanil <tulindanil@gmail.com>
2015-08-24 21:02:07 +0800

    Added ios and moved gitignore

---
+++ Neighborhood-ios/Neighborhood-ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Neighborhood-ios
+//
+//  Created by Danil Tulin on 8/24/15.
+//  Copyright (c) 2015 Danil Tulin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Neighborhood-ios/Neighborhood-ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Neighborhood-ios
+//
+//  Created by Danil Tulin on 8/24/15.
+//  Copyright (c) 2015 Danil Tulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
