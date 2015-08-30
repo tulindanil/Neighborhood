a312d2f7224f816c44f18f18873b422520fa96b0
tulindanil <tulindanil@gmail.com>
2015-08-31 00:48:42 +0300

    Add class

--- Neighborhood-raspberry/script.py
+++ Neighborhood-raspberry/script.py
@@ -1,24 +1,30 @@
 import json, httplib
+import time
 
 temperatureClassName = 'Temperature'
 
+class Hardware:
 
+    def getTemperature():
+
+        filepath = '/sys/devices/w1_bus_master1/28-0000052c4b73/w1_slave'
+        f = open(filepath, 'r')
+        data = f.read()
+        f.close()
+
+        return data.find('t=')[2]
 
 
 if __name__ == '__main__':
-
-    filepath = '/sys/devices/w1_bus_master1/28-0000052c4b73/w1_slave'
-    f = open(filepath, 'r')
-    data = f.read()
-
-    print data
-    sys.sleep(1)
-    print data
+    
+    while 1:
+        print Hardware.getTemperature()
+        time.sleep(2)
 
 #    connection = httplib.HTTPSConnection('api.parse.com', 443)
-#    
+#
 #    try:
-#    
+#
 #        connection.connect()
 #        connection.request('POST', '/1/classes/' + temperatureClassName, json.dumps( {'value': 24.57}), {
 #                           'X-Parse-Application-Id': 'VOB4wXj2mGOjJaqzdhkM701n2ahTSRMqZW6QQ8XU',
