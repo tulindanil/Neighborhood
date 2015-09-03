3340ca529ac5138e749246a85a285eafee1cf610
tulindanil <tulindanil@gmail.com>
2015-09-03 09:59:29 +0300

    Bug fix

--- Neighborhood-raspberry/script.py
+++ Neighborhood-raspberry/script.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
             daemon.restart()
         else:
             print "Unknown command"
-                sys.exit(2)
+            sys.exit(2)
         sys.exit(0)
     else:
         print "usage: %s start|stop|restart" % sys.argv[0]
