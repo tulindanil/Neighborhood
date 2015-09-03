002e133f71b0dbef727ea44c649a98b83b314d9e
tulindanil <tulindanil@gmail.com>
2015-09-03 09:30:55 +0300

    Add ParseWorker

--- Neighborhood-raspberry/script.py
+++ Neighborhood-raspberry/script.py
@@ -3,38 +3,39 @@ import time
 
 temperatureClassName = 'Temperature'
 
-class Hardware:
-
+class hardware:
+    
     @staticmethod
     def getTemperature():
-
+        
         filepath = '/sys/devices/w1_bus_master1/28-0000052c4b73/w1_slave'
         f = open(filepath, 'r')
         data = f.read()
         f.close()
+        
+        return float(data[data.find('t=')+2:])/1000
 
-        return data[data.find('t=')+2]
+class ParseWorker:
+
+    parseapi = 'api.parse.com'
+
+    tail = {'X-Parse-Application-Id': 'VOB4wXj2mGOjJaqzdhkM701n2ahTSRMqZW6QQ8XU', 'X-Parse-REST-API-Key': 'v7WQplcOjunw6bTEM4P73k8P4HJqeiNenDxggrtw', 'Content-Type': 'application/json'}
+
+    def __init__(self):
+
+        self.connection = httplib.HTTPSConnection('api.parse.com', 443)
+        self.connection.connect()
+
+    def pushTemperatureValue(self, value):
+
+        self.connect.request('POST', '/1/classes/' + temperatureClassName, json.dumps( {'value': value}), self.tail)
 
 
 if __name__ == '__main__':
     
     while 1:
-        print Hardware.getTemperature()
+        print hardware.getTemperature()
         time.sleep(2)
 
-#    connection = httplib.HTTPSConnection('api.parse.com', 443)
-#
-#    try:
-#
-#        connection.connect()
-#        connection.request('POST', '/1/classes/' + temperatureClassName, json.dumps( {'value': 24.57}), {
-#                           'X-Parse-Application-Id': 'VOB4wXj2mGOjJaqzdhkM701n2ahTSRMqZW6QQ8XU',
-#                           'X-Parse-REST-API-Key': 'v7WQplcOjunw6bTEM4P73k8P4HJqeiNenDxggrtw',
-#                           'Content-Type': 'application/json'
-#                           })
-#        results = json.loads(connection.getresponse().read())
-#        print results
-#
-#    except:
-#
-#        print 'No internet connection'
+    worker = ParseWorker()
+    worker.pushTemperatureValue(hardware.getTemperature())
